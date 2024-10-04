75b18361e2ea44dc564e4ebf11bdc474be0b3f1a
Bela <icerqueira440@gmail.com>
2024-10-04 17:03:15 -0300

    arrumando as cores do tema claro(light) e tema escuro(dark)

--- site/src/components/EnsinoRemoto/EnsinoRemoto.jsx
+++ site/src/components/EnsinoRemoto/EnsinoRemoto.jsx
@@ -6,15 +6,19 @@ import RobertinhoPiscando from "../../assets/robertinhoPiscando.png";
 const EnsinoRemoto = () => {
   return (
     <>
-      <div className="container">
-        <div className="Robertofalando box d-flex justify-content-center align-items-center">
-          <img src={RobertinhoPiscando} alt="" className="robertoPiscando" />
-        </div>
-        <div className="fala-R box">
-          <p>Olá, eu sou o Roberto Robô, venha conhecer o meu site!</p>
+      <div className="d-flex Robertofalando align-items-end justify-content-end">
+        <div className="row m-0 p-0 justify-content-center align-items-center mb-0">
+          <div className="col-4 ms-3  d-flex justify-content-center align-items-center">
+            <img src={RobertinhoPiscando} alt="" className="robertoPiscando" />
+          </div>
+          <div className="col m-0 me-3 d-flex justify-content-center align-items-center fala-R">
+            <p className="m-0">
+              Olá, eu sou o Roberto Robô, venha conhecer o meu site!
+            </p>
+          </div>
         </div>
       </div>
-      <div className="login d-grid gap-3">
+      <div className="login d-grid gap-3 mt-5">
         <Link to={"../EntrarConta"} className="EntrarConta btn btn-primary-e ">
           <p>Entre com sua conta</p>
         </Link>
--- site/src/components/Home/Home.jsx
+++ site/src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
 import Header from "../Header/Header";
@@ -8,36 +8,113 @@ import EnsinoInstitucional from "../../assets/EnsinoInstitucional.png";
 import Separador from "../../assets/linha.svg";
 import SetinhaVoltar from "../../assets/setinhaVoltar.svg";
 
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+
 const Home = () => {
+
+//   const [questions, setQuestions] = useState([]);
+//   const [currentQuestion, setCurrentQuestion] = useState(0);
+//   const [selectedAnswer, setSelectedAnswer] = useState('');
+//   const [isCorrect, setIsCorrect] = useState(null);
+
+//   // Função para carregar as questões de matemática da API do ENEM
+//   const fetchMathQuestions = async () => {
+//     try {
+//       const response = await axios.get('https://api.enem.dev/v1/questions', {
+//         params: {
+//           subject: 'matematica' // Filtra por questões de matemática
+//         }
+//       });
+//       setQuestions(response.data);
+//     } catch (error) {
+//       console.error('Erro ao carregar questões:', error);
+//     }
+//   };
+
+//   // UseEffect para carregar as questões ao montar o componente
+//   useEffect(() => {
+//     fetchMathQuestions();
+//   }, []);
+
+//   // Função para verificar a resposta
+//   const handleAnswerSubmit = () => {
+//     const correctAnswer = questions[currentQuestion].correct;
+//     setIsCorrect(selectedAnswer === correctAnswer);
+//   };
+
+//   // Função para avançar para a próxima pergunta
+//   const nextQuestion = () => {
+//     setIsCorrect(null);
+//     setSelectedAnswer('');
+//     setCurrentQuestion(currentQuestion + 1);
+//   };
+
+//   // Se ainda não carregou as questões, mostra um carregamento
+//   if (questions.length === 0) {
+//     return <div>Carregando questões de matemática...</div>;
+//   }
+
   return (
     <>
-      <Header />
-      <Body />
-      <div className="row">
-        <nav className="gap-4 box-e d-flex flex-column d-md-block col col-md justify-content-center rounded-3">
-          <Link
-            className="ensinoRemoto btn bg-light text-primary-emphasis border"
-            to={"../EnsinoRemoto"}
-          >
-            <img src={ensinoRemoto} alt="" />
-            Ensino Remoto
-          </Link>
-          <Link
-            className="ensinoInstitucional btn bg-light text-primary-emphasis border"
-            to={"EnsinoInstitucional"}
-          >
-            <img src={EnsinoInstitucional} alt="" />
-            Ensino Institucional
-          </Link>
-        </nav>
+        <Header />
+        <Body />
+        <div id="row">
+          <nav className="gap-4 box-e d-flex flex-column d-md-block col col-md justify-content-center rounded-3">
+            <Link
+              className="ensinoRemoto btn bg-light text-primary-emphasis border"
+              to={"../EnsinoRemoto"}
+            >
+              <img src={ensinoRemoto} alt="" />
+              Ensino Remoto
+            </Link>
+            <Link
+              className="ensinoInstitucional btn bg-light text-primary-emphasis border"
+              to={"EnsinoInstitucional"}
+            >
+              <img src={EnsinoInstitucional} alt="" />
+              Ensino Institucional
+            </Link>
+          </nav>
 
-        <img src={Separador} alt="" className="separador" />
-        <div className="rodape d-flex justify-content-center align-items-center pt-4">
-          <img src={SetinhaVoltar} alt="" className="setinha" />
-          <button>prosseguir</button>
+          <img src={Separador} alt="" className="separador" />
+          <div className="rodape d-flex justify-content-center align-items-center pt-4">
+            <img src={SetinhaVoltar} alt="" className="setinha" />
+            <button>prosseguir</button>
+          </div>
         </div>
+</>
+
+
+
+  
+    /* <div>
+      <h1>Questão de Matemática</h1>
+      <div>
+        <p>{questions[currentQuestion].statement}</p>
+        {questions[currentQuestion].options.map((option, index) => (
+          <div key={index}>
+            <input
+              type="radio"
+              id={`option-${index}`}
+              name="answer"
+              value={option}
+              checked={selectedAnswer === option}
+              onChange={(e) => setSelectedAnswer(e.target.value)}
+            />
+            <label htmlFor={`option-${index}`}>{option}</label>
+          </div>
+        ))}
+        <button onClick={handleAnswerSubmit}>Verificar Resposta</button>
+        {isCorrect !== null && (
+          <div>
+            {isCorrect ? <p>Resposta correta!</p> : <p>Resposta errada!</p>}
+            <button onClick={nextQuestion}>Próxima questão</button>
+          </div>
+        )}
       </div>
-    </>
+    </div>
+   </> */
   );
 };
 
