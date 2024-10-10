85a1bcbd5a9fa32424ab9ec0821be4c633f1b901
Karol-ss <karolaynesouzasantos80@gmail.com>
2024-10-10 16:16:48 -0300

    começando o intitucional

--- site/src/components/EnsinoInstitucional/EnsinoInstitucional.jsx
+++ site/src/components/EnsinoInstitucional/EnsinoInstitucional.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import RobertinhoPiscando from "../../assets/robertinhoPiscando.png";
 
 const EnsinoInstitucional = () => {
   return (
-    <div>EnsinoInstitucional</div>
+    <div className="d-flex Robertofalando align-items-end justify-content-end">
+    <div className="row m-0 p-0 justify-content-center align-items-center mb-0">
+      <div className="col-4 ms-3  d-flex justify-content-center align-items-center">
+        <img src={RobertinhoPiscando} alt="" className="robertoPiscando" />
+      </div>
+      <div className="col m-0 me-3 d-flex justify-content-center align-items-center fala-R">
+        <p className="m-0">
+          Olá, eu sou o Roberto Robô, venha conhecer o meu site!
+        </p>
+      </div>
+    </div>
+  </div>
   )
 }
 
