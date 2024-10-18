12402bdd13fa511ae897c4ef190460122dc668b0
Bela <icerqueira440@gmail.com>
2024-10-18 16:35:32 -0300

    ajustando e colocando icons do bootstrap

--- site/src/components/Beneficios/Beneficios.jsx
+++ site/src/components/Beneficios/Beneficios.jsx
@@ -3,11 +3,8 @@ import "./Beneficios.css";
 import sequencia from "../../assets/linhaSequencia.svg";
 import setinha from "../../assets/setaSequencia.svg";
 import RobertinhoPiscando from "../../assets/robertinhoPiscando.png";
-import Jornal from "../../assets/iconJornal.svg";
 import Abajur from "../../assets/abajur.svg";
-import Relogio from "../../assets/Relogio.svg";
 import Dedo from "../../assets/iconDedo.svg";
-import Dinheiro from "../../assets/iconDinheiro.svg";
 import Separador from "../../assets/linha.svg";
 import SetinhaVoltar from "../../assets/setinhaVoltar.svg";
 import { Link } from "react-router-dom";
@@ -31,24 +28,24 @@ const Beneficios = () => {
       </div>
       <div className="Beneficios d-flex justify-content-center flex-column align-items-center mt-5">
         <div className="Benef mt-1">
-          <div className="image">
-            <img src={Jornal} alt="" />
+          <div className="icons">
+          <i className="bi bi-journal-bookmark Journal"></i>
           </div>
           <div className="FalaBene">
             <p>Abre novas possibilidades de estudos na sala de aula</p>
           </div>
         </div>
         <div className="Benef">
-          <div className="image">
-            <img src={Abajur} alt="" />
+          <div className="icons">
+          <i className="bi bi-lamp-fill Lamp"></i>
           </div>
           <div className="FalaBeneAbajur">
             <p>Gestão educacional mais eficiente</p>
           </div>
         </div>
         <div className="Benef">
-          <div className="image">
-            <img src={Relogio} alt="" />
+          <div className="icons">
+          <i className="bi bi-alarm-fill Clock"></i>
           </div>
           <div className="FalaBeneRelogio">
             <p className="ps-2 pe-2">
@@ -57,16 +54,16 @@ const Beneficios = () => {
           </div>
         </div>
         <div className="Benef">
-          <div className="image">
-            <img src={Dedo} alt="" />
+          <div className="icons">
+          <i className="bi bi-hand-index-thumb Finger"></i>
           </div>
           <div className="FalaBeneDedo">
             <p>Aprendizagem mais interativa e dinâmica</p>
           </div>
         </div>
         <div className="Benef">
-          <div className="image">
-            <img src={Dinheiro} alt="" />
+          <div className="icons">
+          <i className="bi bi-coin Money"></i>
           </div>
           <div className="FalaBeneDinheiro">
             <p>Redução de custos com tarefas manuais</p>
