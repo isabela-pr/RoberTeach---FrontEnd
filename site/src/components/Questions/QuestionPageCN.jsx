b838b0749144195af19bb86b818410b3c92b152d
Prof-Castello <gustavo.castello@docente.senai.br>
2024-12-03 14:53:03 -0300

    ajustes balao robertinho

--- site/src/components/Questions/QuestionPageCN.jsx
+++ site/src/components/Questions/QuestionPageCN.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Question from "../Question/Question";
 import ProgressBar from "../ProgressBar/ProgressBar";
 
