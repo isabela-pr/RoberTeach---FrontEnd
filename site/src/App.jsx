a1035acbcc4689fd48d1840ced5124531466efd6
Prof-Castello <gustavo.castello@docente.senai.br>
2024-12-03 14:31:33 -0300

    ajustes

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,27 +1,18 @@
-// import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import QuestionPage from "./QuestionPage";
+// import QuestionPage from "./QuestionPage";
 
 function App() {
   return (
     <>
-      <div>
-        <QuestionPage />
-      </div>
-      {/* <Outlet /> */}
+      {/* <QuestionPage /> */}
+      <Outlet />
     </>
   );
 }
-// function App() {
-//   return (
-//     <>
-//       <Outlet />
-//     </>
-//   );
-// }
 
 export default App;
